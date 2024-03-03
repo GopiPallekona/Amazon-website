d0df3280c80071bfca5232bf87fbb083f71d6648
GopiPallekona <161458266+GopiPallekona@users.noreply.github.com>
2024-03-03 20:39:46 +0530

    Add files via upload
    commiting amazon code

---
+++ data/amazon.js
@@ -0,0 +1,111 @@
+
+
+let productHtml='';
+
+products.forEach((product)=> {
+    productHtml=productHtml+` <div class="product-container">
+    <div class="product-image-container">
+      <img class="product-image"
+        src="${product.image}">
+    </div>
+
+    <div class="product-name limit-text-to-2-lines">
+      ${product.name}
+    </div>
+
+    <div class="product-rating-container">
+      <img class="product-rating-stars"
+        src="images/ratings/rating-${product.rating.stars*10}.png">
+      <div class="product-rating-count link-primary">
+        ${product.rating.count}
+      </div>
+    </div>
+
+    <div class="product-price">
+      ${product.priceCents/100}
+    </div>
+
+    <div class="product-quantity-container">
+      <select>
+        <option selected value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart">
+      <img src="images/icons/checkmark.png">
+      Added
+    </div>
+
+    <button class="add-to-cart-button button-primary js-add-to-cart">
+      Add to Cart
+    </button>
+  </div>`
+    
+})
+
+
+document.querySelector(".js-product-grid").innerHTML=productHtml
+
+document.querySelectorAll("js-add-to-cart").forEach((button)=>{
+  button.addEventListener('click',() =>{
+    const productName=button.dataset.productName;
+
+
+    let matchingItem;
+    createImageBitmap.forEach((item)=>{
+      if (productName===item.productName) {
+        matchingItem=item;
+        
+      }
+    })
+
+    if (matchingItem) {
+      matchingItem.quantity+=1;
+      
+    }
+    else{
+      cart.push({
+        productName:productName,
+        quantity:1
+      })
+    }
+
+    let carQuantity=0;
+    cart.forEach((item)=>{
+      carQuantity += item.quantity;
+    })
+
+    document.querySelector(".js-cart-quantity").innerHTML=carQuantity
+
+  });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
